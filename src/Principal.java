266421529cffa4e5cc8aa3c8b5e0c5af8bd739b7
paumaso <paumartinezsoria@gmail.com>
2024-03-15 16:01:42 +0100

    provamain1

--- src/Principal.java
+++ src/Principal.java
@@ -5,10 +5,10 @@ public class Principal {
 	public static void main(String[] args) {
 		Factorial fact = new Factorial();
 		try {
-			double valor = fact.factorialIteratiu(4);
-			System.out.println(valor);
-		} catch (IllegalArgumentException e) {
+			double valor = fact.factorialRecursiu(3); System.out.println(valor);
+			} catch (IllegalArgumentException e) {
 			System.out.println("No es poden fer servir nombres negatius.");
-		}
+			}
 	}
+	
 }
